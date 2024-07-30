f79f54c1604be725a54ad1cd6353957c68b020e5
aththas <aththasrizwan123@gmail.com>
2024-07-30 10:47:29 +0530

    Added a logger file using slf4j with logback

--- src/main/java/com/mobitel/data_management/auth/service/impl/PasswordServiceImpl.java
+++ src/main/java/com/mobitel/data_management/auth/service/impl/PasswordServiceImpl.java
@@ -29,27 +29,34 @@ public class PasswordServiceImpl implements PasswordService {
             try{
                 final String email = ((User) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getEmail();
                 Optional<User> optionalUser = userRepository.findByEmail(email);
-                if(optionalUser.isEmpty())
+                if(optionalUser.isEmpty()){
+                    log.error("Invalid Authentication for Password Reset: User Not Found");
                     return new ResponseEntity<>("Invalid Authentication", HttpStatus.UNAUTHORIZED);
+                }
 
                 User user = optionalUser.get();
 
                 if(!passwordEncoder.matches(passwordResetDto.getPassword(), user.getPassword())){
+                    log.error("Password Reset: Password Verification Error");
                     return new ResponseEntity<>("Password Verification Error",HttpStatus.OK);
                 }
 
                 if(!passwordResetDto.getNewPassword().equals(passwordResetDto.getConfirmPassword())){
+                    log.error("Password Reset: Password Confirmation Error");
                     return new ResponseEntity<>("Password Confirmation Error",HttpStatus.OK);
                 }
 
                 user.setPassword(passwordEncoder.encode(passwordResetDto.getNewPassword()));
                 userRepository.save(user);
+
+                log.info("Password Reset: Password Updated for user " + user.getEmail());
                 return new ResponseEntity<>("Password Updated",HttpStatus.OK);
             } catch (Exception e){
-                log.error(e.toString());
+                log.error("Password Reset: " + e);
                 return new ResponseEntity<>("Server Error",HttpStatus.INTERNAL_SERVER_ERROR);
             }
         }else{
+            log.error("Password Reset: passwordResetDto object is null");
             return new ResponseEntity<>("Null Values Not Permitted",HttpStatus.OK);
         }
 
