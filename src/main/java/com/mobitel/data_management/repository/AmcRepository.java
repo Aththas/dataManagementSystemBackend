c6c4eb3ee1334b5ebf3bab93d2c06ff9c79295ba
aththas <aththasrizwan123@gmail.com>
2024-07-30 22:38:45 +0530

    AMC View List (ALL and MY) with pagination and sorting

--- src/main/java/com/mobitel/data_management/repository/AmcRepository.java
+++ src/main/java/com/mobitel/data_management/repository/AmcRepository.java
@@ -2,6 +2,8 @@ package com.mobitel.data_management.repository;
 
 import com.mobitel.data_management.auth.entity.user.User;
 import com.mobitel.data_management.entity.Amc;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,7 +13,7 @@ import java.util.Optional;
 @Repository
 public interface AmcRepository extends JpaRepository<Amc,Integer> {
     Optional<Amc> findByContractName(String contractName);
-    List<Amc> findAllByOrderByIdAsc();
+    Page<Amc> findAll(Pageable pageable);
+    Page<Amc> findAllByUserId(Integer userId,Pageable pageable);
 
-    List<Amc> findAllByUserIdOrderByIdAsc(Integer userId);
 }
